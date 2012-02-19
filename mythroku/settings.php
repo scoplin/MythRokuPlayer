45b6ae5fe2b37d99875b18fda5383d787f970038
Scott Coplin <coplin.6@osu.edu>
2012-02-18 21:34:27 -0500

    Configuration improvements
    Moved configuration to an external file (actually one of several files
    in a list that is checked in order).  This will keep users from having
    to edit program code in-place to affect configuration changes.

--- mythroku/settings.php
+++ mythroku/settings.php
@@ -1,15 +1,26 @@
 <?php
 require_once 'php-activerecord/ActiveRecord.php';
 
-$WebServer = "http://192.168.1.200/mythweb";   // include path to mythweb eg, http://yourip/mythweb
-$MythRokuDir = "mythroku";				       // name of your mythroku directory in the mythweb folder
-$RokuDisplayType = "SD";				       // set to the same as your Roku player under display type, HD or SD  
-$BitRate = "1500";					           // bit rate of endcoded streams
-$MysqlServer = "192.168.1.200";                // mysql server ip/name
-$MythTVdb = "mythconverg";                     // mythtv database name
-$MythTVdbuser = "mythtv";                      // mythtv database user
-$MythTVdbpass = "mythtv";                      // mythtv database password
-$MythTVvideos = "/data/Videos/";			   // mythweb/data symbolic link to the storage group for mythvideos 
+# Borrowed list of config files from classes/MythTV.php
+# Note that the web user must be able to read the config file
+# Typically this means they need to be added to the mythtv group
+$ConfigFiles = array( '/usr/local/share/mythtv/mysql.txt',
+                      '/usr/share/mythtv/mysql.txt',
+                      '/usr/local/etc/mythtv/mysql.txt',
+                      '/etc/mythtv/mysql.txt',
+                      'mysql.txt' );
+
+$config = load_config(find_config($ConfigFiles));
+$localhost = $_SERVER['SERVER_ADDR'];
+$WebServer = "http://$localhost/mythweb";      // include path to mythweb eg, http://yourip/mythweb
+$MythRokuDir = "mythroku";                     // name of your mythroku directory in the mythweb folder
+$RokuDisplayType = "HD";                       // set to the same as your Roku player under display type, HD or SD  
+$BitRate = "1500";                             // bit rate of endcoded streams
+$MysqlServer = $config['DBHostName'];  // mysql server ip/name
+$MythTVdb = $config['DBName'];         // mythtv database name
+$MythTVdbuser = $config['DBUserName']; // mythtv database user
+$MythTVdbpass = $config['DBPassword']; // mythtv database password
+$MythTVvideos = "/data/videos/";               // mythweb/data symbolic link to the storage group for mythvideos 
 
 /* can use TimeOffset in mythconver.settings */
 //date_default_timezone_set ( 'GMT' );
@@ -57,13 +68,46 @@ class VideoCategory extends ActiveRecord\Model
 //function to convert mysql timestamp to unix time
 function convert_datetime($str) 
 {
-	list($date, $time) = explode(' ', $str);
-	list($year, $month, $day) = explode('-', $date);
-	list($hour, $minute, $second) = explode(':', $time);
+    list($date, $time) = explode(' ', $str);
+    list($year, $month, $day) = explode('-', $date);
+    list($hour, $minute, $second) = explode(':', $time);
 
-	$timestamp = mktime($hour, $minute, $second, $month, $day, $year);
+    $timestamp = mktime($hour, $minute, $second, $month, $day, $year);
 
-	return $timestamp;
+    return $timestamp;
+}
+
+function load_config($source)
+{
+    $config = array();
+    $file = fopen($source, 'r');
+    if (!$file) {
+        $id = `whoami`;
+        die("Failed to open $source as $id");
+    }
+    while (!feof($file)) {
+        $line = trim(fgets($file));
+        // skip comments and empty lines!
+        if (strlen($line) === 0 || strpos($line, '#') === 0)
+            continue;
+        list ($public, $value) = explode('=', $line, 2);
+        $config[$public] = $value;
+    }
+    fclose($file);
+    return $config;
+}
+
+function find_config($configs)
+{
+    foreach ($configs as $config) {
+        $config = realpath($config);
+        if ($config) {
+            return $config;
+	    break;
+        }
+    }
+
+    die("Cannot find a valid config file.\n");
 }
 
 ?>
