30046330e8921bcfaa3529fdec4b9110dfc70e02
Scott Coplin <coplin.6@osu.edu>
2012-02-20 22:25:43 -0500

    Allow for re-run without reset

--- bin/rokuencode2.sh
+++ bin/rokuencode2.sh
@@ -20,7 +20,7 @@ function usage {
         echo
     fi
     cat >&2 <<EOL
-$(basename $0) [ -n | --dry-run ] <mythdir> <mpg file> <command> [ <jobid> ]
+$(basename $0) [ -n | --dry-run ] <command> <mpg file> [ <jobid> ]
 
 $(basename $0) will attempt to encode an mpeg file to mp4
 using the handbrake CLI.  It will also optionally update
@@ -41,12 +41,11 @@ if [[ "-h" = "$1" || "--help" = "$1" ]]; then
     usage
 fi
 
-[[ $# -eq 3 ]] || [[ $# -eq 4 ]] || usage "Incorrect number of arguments"
+[[ $# -eq 2 ]] || [[ $# -eq 3 ]] || usage "Incorrect number of arguments"
 
-MYTHDIR=$1
+COMMAND=$1
 MPGFILE=$2
-COMMAND=$3
-JOBID=$4
+JOBID=$3
 
 # Function to find a readable file in list of possible locations
 function findFile {
@@ -99,11 +98,13 @@ EOL
 UPDATE_DATABASE=${UPDATE_DATABASE:-true}
 REMOVE_ORIGINAL=${REMOVE_ORIGINAL:-false}
 HANDBRAKE_ARGS=${HANDBRAKE_ARGS:-"--preset='iPhone & iPod Touch'"}
-LOGFILE=${LOGFILE:-"/var/log/mythtv/rokuencode.%s.log"}
 GENERATE_PREVIEWS=${GENERATE_PREVIEWS:-true}
 
 # Calculate the base name for the file
-basename=$(echo $MPGFILE | sed 's/\(.*\)\..*/\1/')
+mythdir=$(cd $(dirname $MPGFILE) && pwd)
+basename=$(echo $(basename $MPGFILE) | sed 's/\(.*\)\..*/\1/')
+mpgname=${basename}.mpg
+mp4name=${basename}.mp4
 
 function process_command {
     echo "Configuration is:"
@@ -151,37 +152,36 @@ EOL
 }
 
 function doencode {
-    newname="$MYTHDIR/${basename}.mp4"
-    echo "Roku Encode $MPGFILE to $newname"
+    [[ ! -r "$mythdir/$mpgname" ]] && echo "MPEG file not found" && exit 1
+
+    echo "Encode $mpgname to $mp4name"
     # Translate carriage returns to newlines for the log, and report progress sanely
     set -o pipefail
-    /usr/bin/HandBrakeCLI $HANDBRAKE_ARGS -i $MYTHDIR/$MPGFILE -o $newname | tr '\015' '\n' | handbrake_progress
+    /usr/bin/HandBrakeCLI $HANDBRAKE_ARGS -i "$mythdir/$mpgname" -o "$mythdir/$mp4name" | tr '\015' '\n' | handbrake_progress
     [[ $? -eq 0 ]] || exit $?
 
     if [[ "$GENERATE_PREVIEWS" = "true" ]]; then
         echo "Generate Previews"
         # Mythtv seems to have problems with keyframes in mp4s, so make previews with ffmpeg
-        ffmpeg -loglevel quiet -ss 64 -vframes 1 -i $newname -y -f image2  $MYTHDIR/$newname.png
-        ffmpeg -loglevel quiet -ss 64 -vframes 1 -i $newname -y -f image2 -s 100x75 $MYTHDIR/$newname.-1.100x75.png
-        ffmpeg -loglevel quiet -ss 64 -vframes 1 -i $newname -y -f image2 -s 100x56 $MYTHDIR/$newname.-1.100x56.png
-        ffmpeg -loglevel quiet -ss 64 -vframes 1 -i $newname -y -f image2 -s 320x240 $MYTHDIR/$newname.-1.320x240.png
+        ffmpeg -loglevel quiet -ss 64 -vframes 1 -i "$mythdir/$mp4name" -y -f image2  "$mythdir/$mp4name.png"
+        ffmpeg -loglevel quiet -ss 64 -vframes 1 -i "$mythdir/$mp4name" -y -f image2 -s 100x75 "$mythdir/$mp4name.-1.100x75.png"
+        ffmpeg -loglevel quiet -ss 64 -vframes 1 -i "$mythdir/$mp4name" -y -f image2 -s 100x56 "$mythdir/$mp4name.-1.100x56.png"
+        ffmpeg -loglevel quiet -ss 64 -vframes 1 -i "$mythdir/$mp4name" -y -f image2 -s 320x240 "$mythdir/$mp4name.-1.320x240.png"
     fi
 
 
     if [[ "$UPDATE_DATABASE" = "true" ]]; then
         echo "Database/remove"
-        # update the db to point to the mp4
-        NEWFILESIZE=$(du -b "$newname" | cut -f1)
+        # update the db with the new info
+        newfilesize=$(du -b "$mythdir/$mp4name" | cut -f1)
         mysql --user=$DBUserName --password=$DBPassword --host=$DBHostName $DBName <<EOL
 UPDATE recorded
-SET basename='$basename.mp4',filesize='$NEWFILESIZE',transcoded='1'
-WHERE basename='$MPGFILE';
+SET basename='$mp4name',filesize='$newfilesize',transcoded='1'
+WHERE basename='$mpgname' OR basename='$mp4name';
 EOL
         if [[ "$REMOVE_ORIGINAL" = "true" ]]; then
-            rm -f $MYTHDIR/$MPGFILE
-            rm -f $MYTHDIR/$MPGFILE*.png
-        else
-            mv $MYTHDIR/$MPGFILE $MYTHDIR/$MPGFILE.old
+            rm -f "$mythdir/$mpgname"
+            rm -f $mythdir/$mpgname*.png
         fi
     fi
     
@@ -199,31 +199,31 @@ EOL
 
 # Function to restore the original recording in the database
 function doreset {
+    [[ ! -r "$mythdir/$mpgname" ]] && echo "Original not found to restore" && exit 1
+
     # update the db to point to the mpg
-    newname="$MYTHDIR/${basename}.mpg"
-    if [[ "$UPDATE_DATABASE" = "true" && -r "$newname.old" ]]; then
-    mv $newname.old $newname
-    NEWFILESIZE=$(du -b "$newname" | cut -f1)
-    mysql --user=$DBUserName --password=$DBPassword --host=$DBHostName $DBName <<EOL
+    if [[ "$UPDATE_DATABASE" = "true" ]]; then
+        newfilesize=$(du -b "$mythdir/$mpgname" | cut -f1)
+        mysql --user=$DBUserName --password=$DBPassword --host=$DBHostName $DBName <<EOL
 UPDATE recorded
-SET basename='$basename.mpg',filesize='$NEWFILESIZE',transcoded='0'
-WHERE basename='$MPGFILE';
+SET basename='$mpgname',filesize='$newfilesize',transcoded='0'
+WHERE basename='$mp4name';
 EOL
-    rm -f $MYTHDIR/$MPGFILE
-    rm -f $MYTHDIR/$MPGFILE*.png
-fi
+        rm -f "$mythdir/$mp4name"
+        rm -f $mythdir/$mp4name*.png
+    fi
 }
 
 # Set up logging and execute
-if [[ -n "LOGFILE" ]]; then
+if [[ -n "$LOGFILE" ]]; then
     log=$(printf "$LOGFILE" $basename)
     echo "Roku $COMMAND $MPGFILE.  Logging to $log"
     set -o pipefail
     process_command 2>&1 | \
         while read line; do
             echo "$(date --rfc-3339=seconds): $line"
-        done >> $log
+        done >> "$log"
 else
     echo "Roku $COMMAND $MPGFILE.  Logging to console"
-    process_command || exit $?
+    process_command
 fi
